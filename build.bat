f74014d582b80036a6784a17c4e456f8868d41c2
jmscreation <jmscreator@gmail.com>
2021-11-15 14:28:44 -0500

    Standard library update
    Library support updated
    
    Installed new build script

--- build.bat
+++ build.bat
@@ -1,32 +1,85 @@
 @echo off
-REM		Build Script
+::		Custom Build Script 2.0
+::
+::       Custom library support
+::
+::     Set Compiler Settings Here
 
-REM Set Compiler Settings Here
 
-cls
-
+:: Compiler Specification And Build Settings
 set CPP=c++
 set GPP=g++
+set GCC=gcc
 set OUTPUT=program.exe
-set DEBUGMODE=1
-
-set REBUILD_LIBRARIES=0
+set DEBUGMODE=0
+set COMMANDLINE=1
 
 set LINK_ONLY=0
 set VERBOSE=0
 
+set REBUILD_SOURCE_DIRECTORIES=1
+set REBUILD_SOURCE_LIBRARIES=0
 set ASYNC_BUILD=1
 
-set COMPILER_FLAGS=-std=c++20
-set ADDITIONAL_LIBRARIES=-static-libstdc++ -static -lssl -lcrypto -lgdi32 -lcrypt32 -lws2_32
-set ADDITIONAL_LIBDIRS=-Llibraries\openssl\lib
-set ADDITIONAL_INCLUDEDIRS=-Ilibraries -Ilibraries\openssl\include
+:: Configure Source For Compiling And Additional Custom Library Directories / Names
+set SOURCE_DIRECTORIES=src libraries\srclibs
+set INCLUDE_DIRECTORIES=libraries\srclibs
+set LIBRARY_DIRECTORIES=libraries\json-loader\librapidjson libraries\json-loader libraries\libopenssl
+set LIBRARY_NAMES=ssl64 crypto64 gdi32 crypt32 ws2_32
+
+:: Custom Library Support Directory Names
+set LIBRARY_DIRECTORY_NAME=lib\windows
+set INCLUDE_DIRECTORY_NAME=include
+set SOURCE_DIRECTORY_NAME=src
+
+:: Additional Compiler Flags And Configuration Settings
+set CPP_COMPILER_FLAGS=-std=c++20
+set C_COMPILER_FLAGS=
+set OBJECT_DIRECTORY=.objs
+
+:: Advanced / Extra Command Line Settings For Building / Linking
+set ADDITIONAL_INCLUDEDIRS=
+set ADDITIONAL_LIBRARIES=-static-libstdc++ -static-libgcc
+set ADDITIONAL_LIBDIRS=
 
 
-del %OUTPUT% 2>nul
+
+
+:: ---------- Build Script Start -----------
+
+cls
+:: Force current directory to program directory
+
+pushd "%~dp0"
 
 setlocal enabledelayedexpansion
 
+:: Configure Raw MinGW Command Line From Custom Settings
+(for %%D in (%INCLUDE_DIRECTORIES%) do (
+	set ADDITIONAL_INCLUDEDIRS=!ADDITIONAL_INCLUDEDIRS! -I%%D
+))
+
+:: Source directories are separated for libraries
+set LIBRARY_SOURCE_DIRECTORIES=
+
+(for %%D in (%LIBRARY_DIRECTORIES%) do (
+	set ADDITIONAL_INCLUDEDIRS=!ADDITIONAL_INCLUDEDIRS! -I%%D\!INCLUDE_DIRECTORY_NAME!
+	set LIBRARY_SOURCE_DIRECTORIES=!LIBRARY_SOURCE_DIRECTORIES! %%D\!SOURCE_DIRECTORY_NAME!
+	set ADDITIONAL_LIBDIRS=!ADDITIONAL_LIBDIRS! -L%%D\!LIBRARY_DIRECTORY_NAME!
+))
+
+(for %%D in (%LIBRARY_NAMES%) do (
+	set ADDITIONAL_LIBRARIES=!ADDITIONAL_LIBRARIES! -l%%D
+))
+
+:: Debugging Info
+:: echo %ADDITIONAL_INCLUDEDIRS%
+:: echo %ADDITIONAL_LIBDIRS%
+:: echo %ADDITIONAL_LIBRARIES%
+:: echo %SOURCE_DIRECTORIES%
+::----------------------
+
+del %OUTPUT% 2>nul
 
 if %LINK_ONLY% GTR 0 (
 	goto linker
@@ -44,44 +97,71 @@ if %ASYNC_BUILD% GTR 0 (
 	set WAIT=/WAIT
 )
 
-del /S /Q ".objs64\*" 2>nul
+set OBJECT_DIRS=
 
-if %REBUILD_LIBRARIES% GTR 0 (
-	del /S /Q "libraries\*.o" 2>nul
-)
+:: Delete objects from object directories / populate object directories array
+(for %%D in (%SOURCE_DIRECTORIES%) do (
+	if %REBUILD_SOURCE_DIRECTORIES% GTR 0 (
+		del /S /Q "%%D\%OBJECT_DIRECTORY%\*.o" 2>nul
+	)
+	set OBJECT_DIRS=!OBJECT_DIRS! %%D\!OBJECT_DIRECTORY!
+))
 
-if not exist .objs64 (
-	echo Creating Object Directory Structure...
-	mkdir .objs64
-)
+(for %%D in (%LIBRARY_SOURCE_DIRECTORIES%) do (
+	if %REBUILD_SOURCE_LIBRARIES% GTR 0 (
+		del /S /Q "%%D\%OBJECT_DIRECTORY%\*.o" 2>nul
+	)
+	set OBJECT_DIRS=!OBJECT_DIRS! %%D\!OBJECT_DIRECTORY!
+))
 
-echo Building Libraries...
-for %%F in (libraries\*.cpp) do (
-	if not exist libraries\%%~nF.o (
-		echo Building %%~nF.o
-		start /B %WAIT% "%%~nF.o" %CPP% %ADDITIONAL_INCLUDEDIRS% %COMPILER_FLAGS% %DEBUG_INFO% -c %%F -o libraries\%%~nF.o
-
-		if %VERBOSE% GTR 0 (
-			echo %CPP% %ADDITIONAL_INCLUDEDIRS% %COMPILER_FLAGS% %DEBUG_INFO% -c %%F -o libraries\%%~nF.o
+:: Create Object Directory Structure
+(for %%D in (%SOURCE_DIRECTORIES%) do (
+	if exist %%D\ (
+		if not exist %%D\%OBJECT_DIRECTORY% (
+			echo Creating Object Directory Structure...
+			mkdir %%D\%OBJECT_DIRECTORY%
 		)
 	)
-)
+))
 
-echo Building API Files...
-for %%F in (*.cpp) do (
-	if not exist .objs64\%%~nF.o (
-		echo Building %%~nF.o
-		start /B %WAIT% "%%~nF.o" %CPP% %ADDITIONAL_INCLUDEDIRS% %COMPILER_FLAGS% %DEBUG_INFO% -c %%F -o .objs64\%%~nF.o
 
-		if %VERBOSE% GTR 0 (
-			echo %CPP% %ADDITIONAL_INCLUDEDIRS% %COMPILER_FLAGS% %DEBUG_INFO% -c %%F -o .objs64\%%~nF.o
-		)
+(for %%D in (%SOURCE_DIRECTORIES%) do (
+	echo Building Source Files For %%D...
+	if exist %%D\ (
+		call :compile_function %%D cpp %CPP% "%CPP_COMPILER_FLAGS%"
+		call :compile_function %%D c %GCC% "%C_COMPILER_FLAGS%"
+	) else (
+		echo Skipping non-existent directory...
 	)
-)
+))
+goto loop
 
-REM Wait for building process to finish
+:: ---------- Compiler Function -----------
+::	SourceDirctory FileExtention Compiler CompilerFlags
+:compile_function
+	set OBJ_DIR=%1\%OBJECT_DIRECTORY%
+	set /a n=0
+	for /R %1 %%F in (*.%2) do (
+		if not exist !OBJ_DIR!\%%~nF!n!.o (
+			echo Building %~n3_%%~nF!n!.o
+			start /B %WAIT% "%%~nF!n!.o" %3 %ADDITIONAL_INCLUDEDIRS% %~4 %DEBUG_INFO% -c %%F -o !OBJ_DIR!\%~n3_%%~nF!n!.o
+
+			if %VERBOSE% GTR 0 (
+				echo %3 %ADDITIONAL_INCLUDEDIRS% %~4 %DEBUG_INFO% -c %%F -o !OBJ_DIR!\%~n3_%%~nF!n!.o
+			)
+		)
+		set /a n+=1
+	)
+goto close
+::--------------------------------------
+
+:: Wait for building process to finish
 :loop
-for /f %%G in ('tasklist ^| find /c "%CPP%"') do ( set count=%%G )
+set /A count=0
+for /f %%G in ('tasklist ^| find /c "%CPP%"') do ( set /A count+=%%G )
+for /f %%G in ('tasklist ^| find /c "%GCC%"') do ( set /A count+=%%G )
+for /f %%G in ('tasklist ^| find /c "%GPP%"') do ( set /A count+=%%G )
+
 if %count%==0 (
 	goto linker
 ) else (
@@ -92,16 +172,23 @@ if %count%==0 (
 :linker
 
 set "files="
-for /f "delims=" %%A in ('dir /b /a-d ".objs64\%*" ') do set "files=!files! .objs64\%%A"
-for /f "delims=" %%A in ('dir /b /a-d "libraries\*.o" ') do set "files=!files! libraries\%%A"
+
+echo %OBJECT_DIRS%
+
+:: Find All Object Files
+(for %%D in (%OBJECT_DIRS%) do (
+	if exist %%D\ (
+		for /f "delims=" %%A in ('dir /b /a-d "%%D\*.o" ') do set "files=!files! %%D\%%A"
+	)
+))
 
 :link
 echo Linking Executable...
 
-if %DEBUGMODE% GTR 0 (
-	set MWINDOWS=
-) else (
+if %COMMANDLINE% GTR 0 (
 	set MWINDOWS=-mwindows
+) else (
+	set MWINDOWS=
 )
 
 if %VERBOSE% GTR 0 (
@@ -116,3 +203,7 @@ if exist .\%OUTPUT% (
 ) else (
 	echo Build Failed!
 )
+
+
+:: This control is for batch processing functions to return
+:close
---
+++ src/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <string>
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
+#include "windows.h"
+#include "jsonloader.h"
+#include "sqlite3.h"
+
+
+std::string Client(const std::string& host, const std::string& path) {
+    std::string rvalue;
+    httplib::SSLClient client(host.data(), 443);
+
+    client.set_ca_cert_path("certs/ca_list.pem");
+    client.enable_server_certificate_verification(true);
+    int verify = client.get_openssl_verify_result();
+    if(verify){
+        std::cout << X509_verify_cert_error_string(verify) << "\n";
+    }
+    
+    auto response = client.Get(path.data());
+    if(response){
+        if(response->status == 200){
+            rvalue = response->body;
+        } else {
+            std::cout << "Status: " << response->status << "\n";
+        }
+    } else {
+        std::cout << "Invalid Request\n";
+    }
+
+    return rvalue;
+}
+
+
+struct ResponseMessage {
+    int id;
+    std::string info;
+    std::string request;
+};
+
+std::string Serialize(const ResponseMessage& msg) {
+    rapidjson::Document doc;
+
+    doc.SetObject();
+
+    doc.AddMember("info", rapidjson::Value(rapidjson::StringRef(msg.info.data())).Move(), doc.GetAllocator());
+    doc.AddMember("request", rapidjson::Value(rapidjson::StringRef(msg.request.data())).Move(), doc.GetAllocator());
+    doc.AddMember("id", rapidjson::Value(msg.id).Move(), doc.GetAllocator());
+
+
+    rapidjson::StringBuffer buf;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buf);
+    doc.Accept(writer);
+
+    std::string result(buf.GetString(), buf.GetSize());
+
+    return result;
+}
+
+bool GetResponse(const std::string& request, std::string& response) {
+    ResponseMessage msg;
+    msg.id = 100;
+    msg.info = "Test Information";
+    msg.request = request;
+
+    response = Serialize(msg);
+
+    std::cout << response << "\n";
+
+    return true;
+}
+
+void Server() {
+    httplib::SSLServer server("certs/server.pem", "certs/server.key");
+
+    server.set_keep_alive_max_count(2);
+    server.set_keep_alive_timeout(15);
+
+    server.set_pre_routing_handler([](const httplib::Request &req, httplib::Response &res) -> httplib::SSLServer::HandlerResponse {
+        std::cout << "from -> " << req.remote_addr << ":" << req.remote_port << "\n";
+        return httplib::SSLServer::HandlerResponse::Unhandled;
+    });
+
+    server.Post("/api", [](const httplib::Request &req, httplib::Response &res){
+
+        if(req.get_header_value("Content-Type") != "text/api-request"){
+            res.status = 404;
+            return httplib::SSLServer::HandlerResponse::Handled;
+        }
+        
+        std::string response;
+
+        if(!GetResponse(req.body, response)){
+            res.status = 500;
+            return httplib::SSLServer::HandlerResponse::Handled;
+        }
+
+        res.set_content(response, "text/api-response");
+        return httplib::SSLServer::HandlerResponse::Handled;
+    });
+
+    server.set_mount_point("/", "./html");
+
+    server.listen("0.0.0.0", 443);
+
+    if(!server.is_running()){
+        std::cout << "Server failed to start\n";
+    }
+}
+
+
+int main() {
+    Server();
+    
+    
+    return 0;
+}
